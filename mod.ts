87095430e58daf0341a49d63696aaf2fbbccf70a
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-03 13:21:45 +0530

    feat: add `Affine`

--- mod.ts
+++ mod.ts
@@ -6,6 +6,8 @@ import init, {
   from_adfgvx as wasm_from_adfgvx,
   adfgx as wasm_to_adfgx,
   from_adfgx as wasm_from_adfgx,
+  affine as wasm_to_affine,
+  from_affine as wasm_from_affine,
 } from "./wasm.js";
 
 await init(source);
@@ -32,4 +34,12 @@ export function toADFGX(key: string, keyword: string, text: string): string {
 
 export function fromADFGX(key: string, keyword: string, text: string): string {
   return wasm_from_adfgx(key, keyword, text);
+}
+
+export function toAffine(a: number, b: number, text: string): string {
+  return wasm_to_affine(a, b, text);
+}
+
+export function fromAffine(a: number, b: number, text: string): string {
+  return wasm_from_affine(a, b, text);
 }
