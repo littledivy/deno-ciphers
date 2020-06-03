a0dffd70ec3026fad1a431b97046e8316fb9f328
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-03 12:41:02 +0530

    perf: add OP creating tool

---
+++ tools/createOP.ts
@@ -0,0 +1,21 @@
+export default createOP(fn: string) {
+  return `
+  #[wasm_bindgen]
+  pub fn ${fn.toLowerCase()}(key: String, text: String) -> String {
+      let ciph = ${fn}::new(&key, &keyword);
+      // encipher
+      let ctext = ciph.encipher(&text).unwrap();
+      ctext
+  }
+
+  #[wasm_bindgen]
+  pub fn from_${fn.toLowerCase()}(key: String, text: String) -> String {
+      let ciph = ${fn}::new(&key, &keyword);
+      // decipher
+      let ptext = ciph.decipher(&text).unwrap();
+      ptext
+  }
+  `
+}
+
+createOP(Deno.args[0]);
